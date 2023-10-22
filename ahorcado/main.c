a8c46fccd80d98ed9a0119d45d21a6658c7f1606
lorenzo.canovas <jlorenzocanovas@gmail.com>
2023-10-22 16:18:15 -0300

    Comentando el codigo del juego del ahorcado

--- ahorcado/main.c
+++ ahorcado/main.c
@@ -26,12 +26,16 @@ void asignar_memoria_para_frase_y_palabra(void);
 int main(void) {
 	bool continuar_jugando = true;
 	do {
+		// Usuario selecciona categoria y se le da una palabra al azar a adivinar
 		iniciar_juego();
+		// El usuario juega hasta adivinar o que se le acaben los intentos
 		empezar_partida();
 
+		// Se le pregunta al usuario si desea seguir jugando
 		continuar_jugando = imprimir_pregunta_seguir_jugando();
 	} while (continuar_jugando);
 
+	// El usuario no quiere seguir jugando, por lo que se le imprime mensaje de cierre del juego
 	imprimir_msg_fin();
 
 	return 0;
@@ -78,7 +82,9 @@ void obtener_palabra_a_adivinar(void) {
 }
 
 void iniciar_juego(void) {
+	// El usuario selecciona que tipo de palabra va a adivinar segun ciertas categorias
 	seleccionar_categoria();
+	// Segun la categoria asignada se selecciona al azar una palabra
 	obtener_palabra_a_adivinar();
 
 	DATOS_JUEGO.intentos = 0;
@@ -86,7 +92,7 @@ void iniciar_juego(void) {
 }
 
 void empezar_partida(void) {
-	char letra;
+	char letra; // Variable utilizada para recibir la letra que puede estar en la palabra a adivinar
 	bool partida_terminada = false;
 	bool se_aserto;
 	bool ganado = false;
@@ -94,29 +100,36 @@ void empezar_partida(void) {
 	do {
 		se_aserto = false;
 
+		// Imprime el dibujo del ahorcado, el puntaje, la categoria, la palabra que se esta adivinando
 		imprimir_ahorcado();
 
+		// Limpia cualquier caracter que se alla colocado en el buffer del teclado
 		limpiar_buffer();
 		printf("\n\n Digite una letra: "); scanf(" %c", &letra);
 		letra = toupper(letra);
 
+		// Revisando si la letra se encuentra en la palabra a adivinar
 		se_aserto = hay_acierto(letra);
 
 		if (se_aserto) {
+			// Si hubo hacierto se completa la palabra y se revisa si ya no hay mas letras a adivinar
 			completar_frase(letra);
 			ganado = se_gano();
 		}
 		else {
+			// No hubo acierto, por lo que se le resta puntos y se le quita intentos
 			DATOS_JUEGO.intentos++;
 			DATOS_JUEGO.puntos -= 200;
 		}
 
+		// Si se acabaron los intentos, la partida termina
 		if (DATOS_JUEGO.intentos == MAX_INTENTOS) {
 			imprimir_msg_perdiste();
 
 			partida_terminada = true;
 		}
 
+		// Si se gano la partida termina
 		if (ganado) {
 			imprimir_msg_ganaste();
 
@@ -140,6 +153,7 @@ bool se_gano(void) {
 bool hay_acierto(char letra) {
 	bool encontrado = false;
 
+	// Revisa si la letra se encuentra en la palabra a adivinar
 	if (strchr(DATOS_JUEGO.palabra_a_adivinar, letra) != NULL) {
 		encontrado = true;
 	}
@@ -148,6 +162,8 @@ bool hay_acierto(char letra) {
 }
 
 void completar_frase(char letra) {
+
+	// Busca el indice en el cual la letra se encuentra en la palabra a adivinar para completar la frase
 	for (int i = 0; i < DATOS_JUEGO.longitud; i++) {
 		if (letra == DATOS_JUEGO.palabra_a_adivinar[i]) {
 			DATOS_JUEGO.frase[i] = DATOS_JUEGO.palabra_a_adivinar[i];
