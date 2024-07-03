5c934e375836e2c58a292361c129af7a2e966071
lorenzo.canovas <jlorenzocanovas@gmail.com>
2024-07-02 21:36:37 -0300

    La opción de mostrar todos los contactos funciona

--- 2024/contactos/agenda_contacto.c
+++ 2024/contactos/agenda_contacto.c
@@ -1,10 +1,28 @@
 #include "inc/agenda_contacto.h"
 
 #include <stdio.h>
+
 #include "inc/opciones_agenda.h"
+#include "inc/lista_contacto.h"
+
+static ListaContacto LISTA_CONTACTO;
+
+static void imprimir_contactos() {
+    uint len = lista_contacto_size(LISTA_CONTACTO);
+
+    printf("\nTus contactos guardados (%u):\n", len);
+    for (uint i = 0; i < len; i++) {
+        Contacto contacto_actual = lista_contacto_obtener_at(LISTA_CONTACTO, i);
+
+        printf("--------------- %u -------------------\n", (i + 1u));
+        contacto_imprimir(contacto_actual);
+        printf("-------------------------------------\n");
+    }
+}
 
 void agenda_contacto_iniciar() {
     printf("Se inicio con exito la agenda de contactos.\n");
+    LISTA_CONTACTO = lista_contacto_from_file();
 }
 
 void agenda_contacto_realizar_accion(OpcionesAgenda op) {
@@ -13,7 +31,7 @@ void agenda_contacto_realizar_accion(OpcionesAgenda op) {
 
     switch (op) {
     case OBTENER_TODOS_LOS_CONTACTOS:
-        // TODO
+        imprimir_contactos();
         break;
 
     case OBTENER_UN_CONTACTO:
@@ -33,11 +51,12 @@ void agenda_contacto_realizar_accion(OpcionesAgenda op) {
         break;
 
     case SALIR:
-        // TODO
+        // skip
         break;
     }
 }
 
 void agenda_contacto_destruir() {
     printf("Se destruyo con exito la agenda de contactos.\n");
+    lista_contacto_destruir(&LISTA_CONTACTO);
 }
--- 2024/contactos/main.c
+++ 2024/contactos/main.c
@@ -1,39 +1,23 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-// #include "inc/agenda_contacto.h"
-#include "inc/contacto.h"
-#include "inc/lista_contacto.h"
-// #include "inc/opciones_agenda.h"
-// #include "inc/terminal.h"
+#include "inc/agenda_contacto.h"
+#include "inc/opciones_agenda.h"
+#include "inc/terminal.h"
 
 int main(void) {
-    // agenda_contacto_iniciar();
+    agenda_contacto_iniciar();
 
-    // OpcionesAgenda op;
-    // do {
-    //     op = obtener_opcion();
+    OpcionesAgenda op;
+    do {
+        op = obtener_opcion();
 
-    //     agenda_contacto_realizar_accion(op);
-    //     detener_terminal();
-    // } while (op != SALIR);
+        agenda_contacto_realizar_accion(op);
 
-    // agenda_contacto_destruir();
+        if (op != SALIR) { detener_terminal(); }
+    } while (op != SALIR);
 
-    ListaContacto lc = lista_contacto_from_file();
-
-    printf("Cantidad de contactos en la db: %u\n", lista_contacto_size(lc));
-    if (lista_contacto_size(lc) > 1u) {
-        Contacto yo = lista_contacto_obtener_at(lc, 0u);
-        contacto_imprimir(yo);
-
-        printf("\n");
-
-        Contacto tete = lista_contacto_obtener_at(lc, 1u);
-        contacto_imprimir(tete);
-    }
-
-    lista_contacto_destruir(&lc);
+    agenda_contacto_destruir();
 
     return EXIT_SUCCESS;
 }
